31d05b64d20bc80de3babe8504cdbdee5cbcccee
Deimo Dorgu <deimodorgutech@gmail.com>
2024-05-13 03:26:22 -0700

    initial setup

---
+++ src/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+import reactDom from "react-dom";
+
+
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
